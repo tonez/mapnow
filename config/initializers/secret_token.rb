49da1b70670263d26b2f20d6aa019fb57e255ce8
Jeremy Barth <jeremy.barth@gmail.com>
2012-02-25 15:37:16 -0800

    mvp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapnow::Application.config.secret_token = '0bf849cb372612494b166aa603f93a546072b5c840a5bcc55ea2d4a984b6ff87f28ca334c5189672e870cb926ada486edc9fe22872e5c75a678f2adb2aaad7ac'
